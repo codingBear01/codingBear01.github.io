a43e3dad0862607847dddbe05072333f570de05a
mameMan <jokingbear01@gmail.com>
2022-05-26 23:11:00 +0900

    lec12

---
+++ src/components/Main/ProfileImage.tsx
@@ -0,0 +1,18 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+
+const PROFILE_IMAGE_LINK =
+  'https://media.bunjang.co.kr/product/170974774_1_1637816612_w360.jpg'
+
+const ProfileImageWrapper = styled.img`
+  width: 120px;
+  height: 120px;
+  margin-bottom: 30px;
+  border-radius: 50%;
+`
+
+const ProfileImage: FunctionComponent = function () {
+  return <ProfileImageWrapper src={PROFILE_IMAGE_LINK} alt="Profile Image" />
+}
+
+export default ProfileImage
